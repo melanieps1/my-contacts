a42db80a57bfdfcdd025b6ad32da32cd34d8116a
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-10 12:52:04 -0400

    classBranch has code from Janine's class example

--- app/main.js
+++ app/main.js
@@ -1,185 +1,177 @@
-Vue.component( 'contactLine', {
-	template :
-		'<tr>\
-			<td class="buttonColumn">\
-				<button v-on:click="removeContact(id)">\
-					<i class="fa fa-trash-o"></i>\
-				</button>\
-			</td>\
-			<td>\
-				{{ name }}\
-			</td>\
-			<td>\
-				{{ phoneNumber }}\
-			</td>\
-			<td>\
-				{{ email }}\
-			</td>\
-		</tr>',
+var contactApp = new Vue({
 
-	props : [ 'id', 'name', 'phoneNumber', 'email', 'removeContact' ]
+  el: "#contactApp",
+
+  data: {
+
+    newName: '',
+    newPhone: '',
+    newEmail: '',
+
+    attemptingSave: false,
+
+    contactList: [], // this is used to store data
+
+    nextId: 0,
+
+    nextIdSeeds: 5,
+
+    contactSeeds: [
+      {
+        id: 1,
+        name: 'Albus Dumbledore',
+        phone: '859-123-4567',
+        email: 'call.me.al@hogwarts.com'
+      },
+      {
+        id: 2,
+        name: 'Minerva McGonagall',
+        phone: '606-234-9876',
+        email: 'mmcgona@hogwarts.com'
+      },
+      {
+        id: 3,
+        name: 'Severus Snape',
+        phone: '502-444-1010',
+        email: 'potionsmstr@hogwarts.com'
+      },
+      {
+        id: 4,
+        name: 'Rubeus Hagrid',
+        phone: '859-913-2846',
+        email: 'norbertsmommy@hogwarts.com'
+      }
+    ]
+
+  },
+
+  computed: {
+
+    contacts: function() { // this is the sorted version of contactList, for display purposes
+
+      return this.contactList.sort(this.alphabetizeContacts);
+
+    },
+
+    missingName: function() {
+
+      return this.newName === '';
+
+    },
+
+    badPhone: function() {
+
+      var pattern = /^[1-9]\d{2}-\d{3}-\d{4}$/;
+      return !pattern.test(this.newPhone);
+
+    },
+
+    badEmail: function() {
+
+      var pattern = /@/;
+      return !pattern.test(this.newEmail);
+
+    }
+
+  },
+
+  beforeMount: function() {
+
+    this.loadContacts();    
+
+  },
+
+  methods: {
+
+    validateForm: function(event) {
+
+      this.attemptingSave = true;
+      if (this.missingName || this.badPhone || this.badEmail) {
+        event.preventDefault();
+      }
+      else {
+        this.addContact();
+      }
+
+    },
+
+    addContact: function() {
+
+      if (this.newName && this.newPhone && this.newEmail) {
+
+        this.contactList.push({
+          id: this.nextId++,
+          name: this.newName,
+          phone: this.newPhone,
+          email: this.newEmail
+        });
+        this.newName = '';
+        this.newPhone = '';
+        this.newEmail = '';
+        this.attemptingSave = false;
+
+        this.saveContacts();
+
+      }
+      else {
+        // nothing
+      }
+
+    },
+    
+    removeContact: function(id) {
+
+      var index = this.findContact(id);
+      this.contactList.splice(index, 1);
+      this.saveContacts();
+
+    },
+    
+    findContact: function(id) {
+
+      return this.contactList.findIndex(function(contact) {
+        return id === contact.id;
+      });
+
+
+    },
+
+    loadContacts: function() {
+
+      this.nextId = localStorage.getItem('nextId');
+      this.contactList = JSON.parse(localStorage.getItem('contactList'));
+      if (!this.contactList) {
+        this.contactList = this.contactSeeds;
+        this.nextId = this.nextIdSeeds;
+      }
+
+    },
+
+    saveContacts: function() {
+
+      localStorage.setItem('contactList', JSON.stringify(this.contactList));
+      localStorage.setItem('nextId', this.nextId);
+
+    },
+
+    alphabetizeContacts: function(a, b) {
+
+      var aName = a.name.toLowerCase();
+      var bName = b.name.toLowerCase();
+
+      var comparison = 0;
+
+      if (aName > bName) {
+        comparison = 1;
+      }
+      else if (aName < bName) {
+        comparison = -1;
+      }
+
+      return comparison;
+
+    }
+
+  }
+
 });
-
-
-var contactList = new Vue( {
-
-	el: '#contactList',
-
-	data: {
-		newName: '',
-		newPhoneNumber: '',
-		newEmail: '',
-		contacts: [
-			{
-				id: 1,
-				name: 'Albus Dumbledore',
-				phoneNumber: '859-123-4567',
-				email: 'headmaster@hogwarts.edu'
-			},
-			{
-				id: 2,
-				name: 'Minerva McGonagall',
-				phoneNumber: '606-234-9876',
-				email: 'professor.mcgonagall@hogwarts.edu'
-			},
-			{
-				id: 3,
-				name: 'Hermione Granger',
-				phoneNumber: '203-535-9501',
-				email: 'hermione.granger@hogwarts.edu'
-			},
-			{
-				id: 3,
-				name: 'Harry Potter',
-				phoneNumber: '303-859-9933',
-				email: 'theboywholived@hogwarts.edu'
-			},
-		],
-		nextContactId : 4,
-		attemptingSave: false
-	},
-
-	computed: {
-
-		// TO DO: Needs to be fixed, doesn't work
-		abcContacts: function() {
-			return this.contacts.sort(this.alphabetizeContacts);
-		}
-
-	},
-
-	beforeMount: function() {
-
-	},
-
-	methods: {
-		alphabetizeContacts: alphabetizeContacts,
-		addContact: addContact,
-		removeContact: removeContact,
-		findContact: findContact,
-		validatePhoneNumber: validatePhoneNumber,
-		validateEmail: validateEmail,
-		validateName: validateName
-	}
-
-});
-
-function alphabetizeContacts(a, b) {
-
-	var aName = a.name.toLowerCase();
-	var bName = b.name.toLowerCase();
-
-	// -1: a < b
-	// 0: a = b
-	// 1: a > b
-	// We are trying to return one of these three values so the sort algorithm knows how to return the contact list
-
-	if (aName < bName) {
-		return -1;
-	} else if (aName > bName) {
-		return 1;
-	} else {
-		return 0;
-	}
-
-}
-
-// TO DO: Put validation functions into this
-
-function addContact() {
-	if (this.newName !== '' && this.newPhoneNumber !== '' && this.newEmail !== '') {
-		this.contacts.push({
-			id: this.nextContactId++,
-			name: this.newName,
-			phoneNumber: this.newPhoneNumber,
-			email: this.newEmail
-		});
-		this.newName = '';
-		this.newPhoneNumber = '';
-		this.newEmail = '';
-	}
-}
-
-
-function removeContact(id) {
-	var contactIndex = this.findContact(id);
-	this.contacts.splice(contactIndex, 1);
-}
-
-
-function findContact(id) {
-	return this.contacts.findIndex(function(contact) { 
-			return id === contact.id; 
-		});
-}
-
-
-function validateName() {
-
-	var nameRe = /[- a-zA-Z]+$/;
-
-	if (this.newName.match(nameRe)) {
-		document.getElementById("btnSave").disabled = false;
-		return true;
-	} else if (this.newPhoneNumber == '') {
-		document.getElementById("btnSave").disabled = true;
-		return false;
-	} else {
-		document.getElementById("btnSave").disabled = true;
-		return false;
-	}
-}
-
-
-function validatePhoneNumber(newPhoneNumber) {
-
-	var phoneRe = /^[0-9\-\+\s\(\)]*$/;
-
-		if (this.newPhoneNumber.match(phoneRe)) {
-			document.getElementById("btnSave").disabled = false;
-			return true;
-		} else if (this.newPhoneNumber == '') {
-			document.getElementById("btnSave").disabled = true;
-			return false;
-		} else {
-			document.getElementById("btnSave").disabled = true;
-			return false;
-		}
-}
-
-
-function validateEmail() {
-	
-	var emailRe = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	
-	if (this.newEmail.match(emailRe)) {
-		document.getElementById("btnSave").disabled = false;
-		return true;
-	} else if (this.newPhoneNumber == '') {
-		document.getElementById("btnSave").disabled = true;
-		return false;
-	} else {
-		document.getElementById("btnSave").disabled = true;
-		return false;
-	}
-}
